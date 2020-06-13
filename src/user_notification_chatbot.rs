0b7c9a2b4bace1e7c1cd75bc634a1bb0c8f81cd7
Johannes Barthel <johannes.barthel@student.kit.edu>
2020-06-14 00:30:00 +0200

    configurable nickname

--- src/user_notification_chatbot.rs
+++ src/user_notification_chatbot.rs
@@ -1,11 +1,13 @@
-use anyhow::{Context, Result};
-use bbb_api::BbbApi;
+use anyhow::Result;
 use std::sync::mpsc::{channel, Receiver, Sender};
 use tokio::runtime::Runtime;
 
 fn chatbot(recv: Receiver<()>) -> Result<()> {
-    let api = bbb_api::BbbApi::from_bbbconf()?;
+    let mut api = bbb_api::BbbApi::from_bbbconf()?;
     let message: String = std::env::var("FREESWITCH_WATCHDOG_CHAT_MESSAGE").unwrap_or("A technical problem has occured. Please wait two minutes and then rejoin audio (telephone icon). We are sorry for the inconvenience.".to_owned());
+    api.set_bot_nickname(
+        &std::env::var("FREESWITCH_WATCHDOG_BOT_NICKNAME").unwrap_or("system".to_owned())
+    );
     let mut runtime = Runtime::new()?;
     loop {
         recv.recv()?;
